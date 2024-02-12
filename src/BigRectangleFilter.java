fd7cc08869b42d84978cc464695932bc8e5d5d8a
dalto <dalton.m.towe@gmail.com>
2024-02-11 23:26:21 -0500

    Cleaned up code warnings

--- src/BigRectangleFilter.java
+++ src/BigRectangleFilter.java
@@ -2,8 +2,12 @@ import java.awt.Rectangle;
 
 public class BigRectangleFilter implements Filter {
     @Override
-    public boolean accept(Object x) {
-        if (!(x instanceof Rectangle rectangle)) {
+    //accepts a rectangle if its perimeter is greater than 10
+    public boolean accept(Object x)
+    {
+        //if x is not a rectangle, return false
+        if (!(x instanceof Rectangle rectangle))
+        {
             return false;
         }
         return (2 * (rectangle.getWidth() + rectangle.getHeight())) > 10;
--- src/BigRectangleLister.java
+++ src/BigRectangleLister.java
@@ -5,38 +5,53 @@ import java.util.List;
 
 
 
+//import java.awt.Rectangle;
 public class BigRectangleLister {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         List<Rectangle> rectangles = createRectangles();
         List<Rectangle> bigRectangles = collectAll(rectangles, new BigRectangleFilter());
         System.out.println("Large rectangles:");
-        for (Rectangle rectangle : bigRectangles) {
+        for (Rectangle rectangle : bigRectangles)
+        {
             System.out.println(recttostring(rectangle));
         }
     }
 
-
-    public static List<Rectangle> createRectangles() {
+        //creates a list of rectangles
+    public static List<Rectangle> createRectangles()
+    {
         List<Rectangle> rectangles = new ArrayList<>();
         rectangles.add(new Rectangle(2, 5));
         rectangles.add(new Rectangle(8, 12));
         rectangles.add(new Rectangle(4, 5));
         rectangles.add(new Rectangle(6, 9));
+        rectangles.add(new Rectangle(3, 5));
+        rectangles.add(new Rectangle(1, 10));
+        rectangles.add(new Rectangle(6, 5));
+        rectangles.add(new Rectangle(7, 9));
+        rectangles.add(new Rectangle(9, 4));
+        rectangles.add(new Rectangle(7, 8));
         return rectangles;
     }
 
-
-    public static List<Rectangle> collectAll(List<Rectangle> rectangles, Filter filter) {
+    //collects all rectangles that pass the filter
+    public static List<Rectangle> collectAll(List<Rectangle> rectangles, Filter filter)
+    {
         List<Rectangle> result = new ArrayList<>();
-        for (Rectangle rectangle : rectangles) {
-            if (filter.accept(rectangle)) {
+        for (Rectangle rectangle : rectangles)
+        {
+            if (filter.accept(rectangle))
+            {
                 result.add(rectangle);
             }
         }
         return result;
     }
 
-    public static String recttostring(Rectangle rectangle) {
+    //converts a rectangle to a string
+    public static String recttostring(Rectangle rectangle)
+    {
         return "Rectangle[width= " + rectangle.getWidth() + ", height= " + rectangle.getHeight() + "]";
     }
 }
